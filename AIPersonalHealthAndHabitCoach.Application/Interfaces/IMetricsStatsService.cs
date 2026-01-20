046120c02b594bdead19bdb5c0375f6a04d71733
Norbert Domagala <norbert.domagala2005@gmail.com>
2026-01-20 21:45:39 +0100

    Add service

---
+++ AIPersonalHealthAndHabitCoach.Application/Interfaces/IMetricsStatsService.cs
@@ -0,0 +1,11 @@
+﻿using AIPersonalHealthAndHabitCoach.Domain.Dtos;
+using AIPersonalHealthAndHabitCoach.Domain.Enums;
+
+namespace AIPersonalHealthAndHabitCoach.Application.Interfaces
+{
+    public interface IMetricsStatsService
+    {
+        Task<MetricStatsDto> GetMetricByType(DateTime startDate, DateTime endDate, MetricType metricType, CancellationToken cancellationToken);
+        Task<MetricStatsDto> GetMetricsSummary(DateTime startDate, DateTime endDate, CancellationToken cancellationToken);
+    }
+}
